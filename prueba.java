258658e65298227971bb18d96e850f1f497f48f8
Juan Trejo <juanpatp08@gmail.com>
2024-02-04 19:53:02 -0500

    Feat: Agregué los comentarios de como crera una rama

--- prueba.java
+++ prueba.java
@@ -1,1 +1,8 @@
-/* pasos para subir archivos del repo local al remoto */
+/*
+
+--- Pasos para subir archivos del repo local al remoto ---
+
+Para crear una rama nueva: git checkout -b nombreRama (La forma mas corta de crear una rama)
+Besitos :3
+
+*/
